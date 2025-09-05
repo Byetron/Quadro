cf4786960f8f3d9a1bd4d27bb050c6b39ba02566
Byetron <naval.adhya@gmail.com>
2025-09-05 10:17:37 +0530

    Web layout

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("dice").onclick = () => {
+  window.location.href = "html/dice.html";
+};
+document.getElementById("guess").onclick = () => {
+  window.location.href = "html/guess.html";
+};
+document.getElementById("hand").onclick = () => {
+  window.location.href = "html/hand.html";
+};
+document.getElementById("wack").onclick = () => {
+  window.location.href = "html/wack.html";
+};
